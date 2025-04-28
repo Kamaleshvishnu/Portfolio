2c5e4ff0de11422fe17584f8eb0e5aece079aa9b
Kamaleshvishnu <146937907+Kamaleshvishnu@users.noreply.github.com>
2025-04-28 23:06:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting me! I will get back to you soon.');
+    document.getElementById('contactForm').reset();
+  });
